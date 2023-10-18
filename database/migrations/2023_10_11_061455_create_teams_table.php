ad13064631bf5b8f9ed0bee7944572c03473a11f
F308\F308 <D1104181007@gm.lhu.edu.tw>
2023-10-18 14:01:36 +0800

    add founders in teams.

--- database/migrations/2023_10_11_061455_create_teams_table.php
+++ database/migrations/2023_10_11_061455_create_teams_table.php
@@ -18,6 +18,7 @@ class CreateTeamsTable extends Migration
             $table->string('name');
             $table->string('country');
             $table->string('founder');
+            $table->string('founders');
             $table->timestamps();
         });
     }
