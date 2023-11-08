a895d1eeed89ce353ca402ef297f3f647c6c75e6
F308\F308 <a246120453@gmail.com>
2023-11-08 11:17:42 +0800

    upload data type tables.

---
+++ resources/views/products.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <title>
+        This is title.
+    </title>
+    <head>
+        <h1>
+            This is head. </br>
+        </h1>
+    </head>
+    <body>
+        This is body.
+    </body>
+    <h2>
+    </h2>
+</html>
