ad32db185998788555844cc85e7cac1ec68b412b
F308\F308 <a246120453@gmail.com>
2023-11-08 11:23:18 +0800

    rename 「game table」 ->「games table」

--- database/migrations/2023_11_08_032143_create_games_table.php
+++ database/migrations/2023_11_08_032143_create_games_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGameTable extends Migration
+class CreateGamesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -33,6 +33,6 @@ class CreateGameTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Game');
+        Schema::dropIfExists('games');
     }
 }
